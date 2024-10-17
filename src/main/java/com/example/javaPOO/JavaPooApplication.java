df66f2411f9d8fa0fb6fe51716e5e5fb80123690
monalizaloren <monalizaloren1@gmail.com>
2024-10-17 09:03:50 -0300

    first commit

---
+++ src/main/java/com/example/javaPOO/JavaPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaPOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPooApplication.class, args);
+	}
+
+}
